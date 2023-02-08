3df1593210d370107d27efb131016ce7f36d304a
YoshimiShima <yoshimi-ramayana@hotmail.co.jp>
2023-02-08 22:24:51 +0900

    form_created

--- app.vue
+++ app.vue
@@ -1,22 +1,5 @@
 <template>
-  <n-loading-bar-provider>
-    <n-message-provider>
-      <n-notification-provider>
-        <n-dialog-provider>
-          <index />
-        </n-dialog-provider>
-      </n-notification-provider>
-    </n-message-provider>
-  </n-loading-bar-provider>
-  </template>
-
-  <script>
-  import { defineComponent } from "vue";
-  import index from "./pages/index.vue";
-
-  export default defineComponent({
-  components: {
-    index,
-  },
-  });
-  </script>
+  <div>
+    <NuxtPage />
+  </div>
+</template>
