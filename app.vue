b23b7fa6571f3f9226af15a23a5370a797ee8675
YoshimiShima <yoshimi-ramayana@hotmail.co.jp>
2023-02-08 19:47:19 +0900

    fixed_route

--- app.vue
+++ app.vue
@@ -1,4 +1,22 @@
-<template lang="pug">
-div
-  NuxtPage /
-</template>
+<template>
+  <n-loading-bar-provider>
+    <n-message-provider>
+      <n-notification-provider>
+        <n-dialog-provider>
+          <index />
+        </n-dialog-provider>
+      </n-notification-provider>
+    </n-message-provider>
+  </n-loading-bar-provider>
+  </template>
+
+  <script>
+  import { defineComponent } from "vue";
+  import index from "./pages/index.vue";
+
+  export default defineComponent({
+  components: {
+    index,
+  },
+  });
+  </script>
