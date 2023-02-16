cf767b6e342b41b59490aeb1fe894273a02a28ce
YoshimiShima <yoshimi-ramayana@hotmail.co.jp>
2023-02-16 14:32:46 +0900

    work-for-now

--- pages/index.vue
+++ pages/index.vue
@@ -1,54 +1,20 @@
-<template>
-  <NConfigProvider inline-theme-disabled>
-    <NMessageProvider>
-      <NNotificationProvider>
-        <NLoadingBarProvider>
-          <NDialogProvider>
-              NForm
-              <NForm
-              ref="formRef"
-              inline
-              :label-width="100"
-              :model="formValue"
-              :rules="rules"
-              :size="size">
-                <NFormItem label="FormItem">
-                  <NInput />
-                </NFormItem>
-                <NFormItemRow>
-                  
-                </NFormItemRow>
-              </NForm>
-            <Provided />
-          </NDialogProvider>
-        </NLoadingBarProvider>
-      </NNotificationProvider>
-    </NMessageProvider>
-  </NConfigProvider>
+<template lang="pug">
+NConfigProvider
+  NMessageProvider
+    NNotificationProvider
+      h1 input form
+      ProvideData
 </template>
 
 <script lang="ts" setup>
 import {
   NConfigProvider,
-  NButton,
-  NSpace,
-  NA,
-  NDialogProvider,
-  NForm,
-  NFormItem,
-  NFormItemCol,
-  NFormItemGi,
-  NFormItemGridItem,
-  NFormItemRow,
-  NInput,
-  NInputGroup,
-  NInputGroupLabel,
-  NInputNumber,
-  NLoadingBarProvider,
   NMessageProvider,
   NNotificationProvider,
 } from 'naive-ui'
-import Provided from './Provided.vue'
+// @ts-ignore
+import ProvideData from '../components/ProvideData.vue'
+
 
 
 </script>
